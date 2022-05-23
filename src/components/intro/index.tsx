c573f71627a0777691d8fb7cee89794a92349323
Ayush Baral <ayushbaral17@gmail.com>
2022-05-23 22:38:51 +0545

    feat(motion):implement motion on intro

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -1,11 +1,49 @@
 import React from "react";
 import { BsMouse } from "react-icons/bs";
+import { motion } from "framer-motion";
+
+export const leftSlide = {
+  hidden: {
+    opacity: 0,
+    x: "-100vw",
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      // delay: 0.5,
+      duration: 2,
+      ease: "easeOut",
+    },
+  },
+};
+
+export const rightSlide = {
+  hidden: {
+    opacity: 0,
+    x: "100vw",
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      // delay: 0.5,
+      duration: 2,
+      ease: "easeOut",
+    },
+  },
+};
 
 const Intro = () => {
   return (
     <div className="flex flex-col md:flex-row h-screen items-center justify-center mb-20">
-      <div className="flex-1">
-        <div className="p-[50px] h-1/2 ">
+      <motion.div
+        className="flex-1"
+        variants={leftSlide}
+        initial="hidden"
+        animate="visible"
+      >
+        <div className="p-[50px] h-1/2">
           <h2 className="text-3xl font-light">Hello, My Name is </h2>
           <h1 className="text-6xl font-semibold my-8">Ayush</h1>
           <div className="h-[50px] overflow-hidden">
@@ -31,18 +69,28 @@ const Intro = () => {
             contact section on this website.
           </p>
         </div>
-        <div className="text-3xl absolute bottom-4 left-1/4 ">
+        <motion.div
+          className="text-3xl absolute bottom-4 left-1/4"
+          drag
+          dragElastic={0.8}
+          dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+        >
           <BsMouse />
-        </div>
-      </div>
-      <div className="flex-1 h-fit w-full mb-10  md:h-full relative">
+        </motion.div>
+      </motion.div>
+      <motion.div
+        className="flex-1 h-fit w-full mb-10  md:h-full relative"
+        variants={rightSlide}
+        initial="hidden"
+        animate="visible"
+      >
         <div className="clip-left  w-full h-full absolute top-0 right-0 "></div>
         <img
           src="https://www.seekpng.com/png/full/53-537858_monsters-png-1024x1024-monsters-illustration-flat-png.png"
           alt=""
           className="w-full h-full object-contain absolute"
         />
-      </div>
+      </motion.div>
     </div>
   );
 };
