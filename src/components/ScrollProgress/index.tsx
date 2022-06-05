e5ff7099341f94f625843c2b0bc2003fa56d6744
Ayush Baral <ayushbaral17@gmail.com>
2022-06-05 18:55:02 +0545

    fix(scroll):set initial scroll value as 0

--- src/components/ScrollProgress/index.tsx
+++ src/components/ScrollProgress/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 const ScrollProgress = () => {
-  const [position, setPosition] = useState<number>(60);
+  const [position, setPosition] = useState<number>(0);
 
   useEffect(() => {
     const listenToScrollEvent = () => {
