614bf50a78d0303ce88879073f5bf7aae2800285
Ayush Baral <ayushbaral17@gmail.com>
2022-06-09 19:21:45 +0545

    feat(projects):add netflix redesign

--- src/components/ProjectsList/project.ts
+++ src/components/ProjectsList/project.ts
@@ -4,6 +4,7 @@ import hulu from "../../assets/projects/hulu.png";
 import bandb from "../../assets/projects/bandb.png";
 import googleSearch from "../../assets/projects/google-search.png";
 import wordsandpages from "../../assets/projects/words-and-pages.png";
+import netflixRedesign from "../../assets/projects/netflix-redesign.png";
 
 export const PROJECT: ProjectInterface[] = [
   {
@@ -30,5 +31,10 @@ export const PROJECT: ProjectInterface[] = [
     id: 5,
     img: wordsandpages,
     link: "https://words-and-pages.vercel.app/",
+  },
+  {
+    id: 6,
+    img: netflixRedesign,
+    link: "https://netflix-redesign-ayush.vercel.app",
   },
 ];
