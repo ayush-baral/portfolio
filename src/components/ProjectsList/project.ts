806fdc32d216b69f05c9045af49f6ad544e7172b
Ayush <ayushbaral17@gmail.com>
2024-02-28 21:30:50 +0545

    feat: add task tracker in project list

--- src/components/ProjectsList/project.ts
+++ src/components/ProjectsList/project.ts
@@ -1,11 +1,12 @@
-import { ProjectInterface } from '../../interface/project';
-import Todo from '../../assets/projects/todo.png';
-import hulu from '../../assets/projects/hulu.png';
-import bandb from '../../assets/projects/bandb.png';
-import googleSearch from '../../assets/projects/google-search.png';
-import wordsandpages from '../../assets/projects/words-and-pages.png';
-import netflixRedesign from '../../assets/projects/netflix-redesign.png';
-import socialMediaApp from '../../assets/projects/socialMediaApp.png';
+import { ProjectInterface } from "../../interface/project";
+import Todo from "../../assets/projects/todo.png";
+import hulu from "../../assets/projects/hulu.png";
+import bandb from "../../assets/projects/bandb.png";
+import googleSearch from "../../assets/projects/google-search.png";
+import taskTracker from "../../assets/projects/task-tracker.png";
+import wordsandpages from "../../assets/projects/words-and-pages.png";
+import netflixRedesign from "../../assets/projects/netflix-redesign.png";
+import socialMediaApp from "../../assets/projects/socialMediaApp.png";
 
 export const PROJECT: ProjectInterface[] = [
   // {
@@ -16,31 +17,31 @@ export const PROJECT: ProjectInterface[] = [
   {
     id: 2,
     img: hulu,
-    link: 'https://ayush-hulu-clone.vercel.app/',
+    link: "https://ayush-hulu-clone.vercel.app/",
   },
   {
     id: 3,
     img: bandb,
-    link: 'https://bandb.netlify.app/',
+    link: "https://bandb.netlify.app/",
   },
   {
     id: 4,
-    img: googleSearch,
-    link: 'https://google-search-ayush.vercel.app',
+    img: taskTracker,
+    link: "https://task-tracker-ayush-baral.vercel.app",
   },
   {
     id: 5,
     img: wordsandpages,
-    link: 'https://words-and-pages.vercel.app/',
+    link: "https://words-and-pages.vercel.app/",
   },
   {
     id: 6,
     img: netflixRedesign,
-    link: 'https://netflix-redesign-ayush.vercel.app',
+    link: "https://netflix-redesign-ayush.vercel.app",
   },
   {
-    id: 6,
+    id: 7,
     img: socialMediaApp,
-    link: 'https://social-media-app-zeta.vercel.app',
+    link: "https://social-media-app-zeta.vercel.app",
   },
 ];
